a0c83f873450e49a038e286760144e321165985a
riera90 <diegorodriguezriera@gmail.com>
2018-05-26 02:15:50 +0200

    added doxygen comments to DenseGraph, Edge and Point2D

--- pr4/src/classes/Edge.hpp
+++ pr4/src/classes/Edge.hpp
@@ -11,43 +11,145 @@
 
 class Vertex;
 
+
+//!< Definicion de la clase Edge
 class Edge {
 private:
-	Vertex* _vertex1;
-	Vertex* _vertex2;
-	std::string _flag;
+	Vertex* _vertex1; //!< puntero a vertice 1 del lado
+	Vertex* _vertex2;	//!< puntero a vertice 2 del lado
+	std::string _flag;  //!< flag del lado, puede usarse a placer
 
 public:
+	/*!
+		\brief		constructor de la clase DenseGraph
+		\param		vertex1: puntero a vertice 1 del lado
+		\param		vertex2: puntero a vertice 2 del lado
+		\note			funcion inline
+	*/
 	inline Edge (Vertex* vertex1=NULL, Vertex* vertex2=NULL)
 	:_vertex1(vertex1), _vertex2(vertex2) {_flag="";};
+
+	/*!
+		\brief		constructor de copue de la clase DenseGraph
+		\param		edge: lado a copiar
+		\note			la copia no genera vertices nuevos, los reulitiza
+		\note			funcion inline
+	*/
 	inline Edge (Edge& edge)
 	:_vertex1(edge.getVertex1()), _vertex2(edge.getVertex2()) {_flag=getFlag();};
 
+	/*!
+		\brief		modificador de la flag
+		\param		flag: flag nueva
+		\note			funcion inline
+	*/
 	inline void setFlag(std::string flag){_flag=flag;}
+
+	/*!
+		\brief		observador de la flag
+		\return		flag del lado
+		\note			funcion inline
+	*/
 	inline std::string getFlag() const {return _flag;}
 
+	/*!
+		\brief		observador del vertice 1 del lado
+		\return		puntero al vertice 1
+		\note			funcion inline
+	*/
 	inline Vertex* getVertex1() {return _vertex1;};
+
+	/*!
+		\brief		observador del vertice 2 del lado
+		\return		puntero al vertice 2
+		\note			funcion inline
+	*/
 	inline Vertex* getVertex2() {return _vertex2;};
 
+	/*!
+		\brief		observador del vertice 1 del lado
+		\return		puntero al vertice 1
+		\note			funcion inline
+	*/
 	inline void setVertex1(Vertex* vertex) {_vertex1=vertex;};
+
+	/*!
+		\brief		observador del vertice 2 del lado
+		\return		puntero al vertice 2
+		\note			funcion inline
+	*/
 	inline void setVertex2(Vertex* vertex) {_vertex2=vertex;};
 
+	/*!
+		\brief		observador del vertice 1 del lado
+		\return		puntero al vertice 1
+		\note			funcion inline
+	*/
 	inline Vertex* getVertexParent() {return _vertex1;};
+
+	/*!
+		\brief		observador del vertice 2 del lado
+		\return		puntero al vertice 2
+		\note			funcion inline
+	*/
 	inline Vertex* getVertexSon() {return _vertex2;};
 
+	/*!
+		\brief		observador del vertice 1 del lado
+		\return		puntero al vertice 1
+		\note			funcion inline
+	*/
 	inline void setVertexParent(Vertex* vertex) {_vertex1=vertex;};
+
+	/*!
+		\brief		observador del vertice 2 del lado
+		\return		puntero al vertice 2
+		\note			funcion inline
+	*/
 	inline void setVertexSon(Vertex* vertex) {_vertex2=vertex;};
 
+	/*!
+		\brief		observador del vertice opuesto al pasado por parametro
+		\param		baseVertex: puntero a vertice base
+		\return		puntero opuesto
+	*/
 	Vertex* getOtherVertex(Vertex baseVertex);
+
+	/*!
+		\brief		modificador del vertice opuesto al pasado por parametro
+		\param		baseVertex: puntero a vertice base
+		\param		vertex: puntero a vertice opuesto
+	*/
 	bool setOtherVertex(Vertex baseVertex, Vertex* vertex);
 
+	/*!
+		\brief		observador de la distancia entre los dos vertices
+		\return		distancia entre los dos vertices del lado
+	*/
 	float getDistance();
 
+	/*!
+		\brief		operador observador de igualdad
+		\param		edge: lado a comprobar con
+		\return		booleano, verdadero si son iguales, falso en el caso contrario
+	*/
 	bool operator==(Edge& edge);
+
+	/*!
+		\brief		operador observador de desigualdad
+		\param		edge: lado a comprobar con
+		\return		booleano, falso si son iguales, verdadero en el caso contrario
+		\note			funcion inline
+	*/
 	inline bool operator!=(Edge& edge){
 		return (!(*this==edge));
 	};
 
+	/*!
+		\brief		modificador de igualdad
+		\param		edge: lado a igualar con
+		\return		lado
+	*/
 	Edge& operator=(Edge& edge);
 };
 
--- pr4/src/classes/Point2D.hpp
+++ pr4/src/classes/Point2D.hpp
@@ -13,35 +13,108 @@
 
 #include "common.hpp"
 
+//!< Definicion de la clase Point2D
 class Point2D {
 private:
-	float _x;
-	float _y;
+	float _x; //!< coordenada x
+	float _y; //!< coordenada y
 
 public:
-
+	/*!
+		\brief		constructor de la clase Point2D
+		\param		x: coordenada x
+		\param		y: coordenada y
+		\note			funcion inline
+	*/
 	inline Point2D (float x=0, float y=0) : _x(x), _y(y) {};
+
+	/*!
+		\brief		constructor de copia de la clase Point2D
+		\param		point: punto a copiar
+		\note			funcion inline
+	*/
 	inline Point2D (const Point2D& point){*this=point;};
 
+	/*!
+		\brief		observador de la coordenada x
+		\return 	coordenada x
+		\note			funcion inline
+	*/
 	inline float getX() const { return _x; }
+
+	/*!
+		\brief		observador de la coordenada y
+		\return 	coordenada y
+		\note			funcion inline
+	*/
 	inline float getY() const { return _y; }
 
+	/*!
+		\brief		modificador de la coordenada x
+		\param 		x: coordenada x
+		\note			funcion inline
+	*/
 	inline void setX(float x) { _x=x; }
+
+	/*!
+		\brief		modificador de la coordenada y
+		\param 		y: coordenada y
+		\note			funcion inline
+	*/
 	inline void setY(float y) { _y=y; }
 
+	/*!
+		\brief		modificador del punto en formato string
+		\param 		point: string del punto en formato x y
+	*/
 	void SetPointString(std::string point);
+
+	/*!
+		\brief		obsercador del punto en formato string
+		\return		string del punto en formato x y
+		\note			funcion constante
+	*/
 	std::string getPointString() const ;
 
+	/*!
+		\brief		lector de un punto por teclado
+	*/
 	void readPointStdin();
+
+	/*!
+		\brief		escritor de un punto por pantalla
+	*/
 	void writePointStdout() const ;
 
+	/*!
+		\brief		operador observador de igualdad
+		\param		point: punto a comprobar con
+		\return		booleano, verdadero si son iguales, falso en el caso contrario
+	*/
 	bool operator==(const Point2D& point) const ;
+
+	/*!
+		\brief		operador observador de desigualdad
+		\param		point: punto a comprobar con
+		\return		booleano, falso si son iguales, verdadero en el caso contrario
+		\note			funcion inline
+	*/
 	inline bool operator!=(const Point2D& point) const {
 		return (!(*this==point));
 	};
 
+	/*!
+		\brief		modificador de igualdad
+		\param		point: punto a igualar con
+		\return		punto
+	*/
 	Point2D& operator=(const Point2D& point);
 
+	/*!
+		\brief		observador de distancia entre this y el punto pasado por parametro
+		\param		point: punto a comprobar con
+		\return		distancia real entre puntos
+	*/
 	float distance(const Point2D& point) const ;
 };
 
