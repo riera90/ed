7cfc8eb54afd2c52593d47cd44b183ae40803796
riera90 <diegorodriguezriera@gmail.com>
2018-05-26 23:29:16 +0200

    added doxygen

--- pr4/src/classes/Tree.hpp
+++ pr4/src/classes/Tree.hpp
@@ -14,36 +14,111 @@
 //This is a really bad implementation of a tree class, is only use for printing a tree on the screen.
 //Please, don't re-use this class.
 
+//!< Definicion de la clase Tree
 class Tree {
 private:
-	Vertex* _root;
+	Vertex* _root; //!< Cabeza del arbol
 	// Vertex* _current; //not in use
 
 private:
 
 public:
+	/*!
+		\brief		constructor de la clase Tree
+	*/
 	inline Tree () {
 		_root=NULL;
 		// _current=NULL;
 	}
-	Vertex* getRoot() {return _root;}
-	void setRoot(Vertex* root) {_root=root;}
 
+	/*!
+		\brief		Observador de la raiz
+		\return		untero a vertice Vertex
+		\note			funcion inline
+	*/
+	inline Vertex* getRoot() {return _root;}
+
+	/*!
+		\brief		modificador de la raiz
+		\param		root: puntero a vertice Vertex
+		\note			funcion inline
+	*/
+	inline void setRoot(Vertex* root) {_root=root;}
+
+	/*!
+		\brief		observador del padre del vertice
+		\param		SonNode: puntero a vertice Vertex
+		\return		puntero a vertice del padre del vertice pasado por parametro
+	*/
 	Vertex* getParent(Vertex* SonNode) ;
+
+	/*!
+		\brief		observador de los hijos del vertice pasado por parametro
+		\param		parentNode: puntero a vertice Vertex
+		\return		lista de punteros a vertices
+	*/
 	std::vector<Vertex*> getSons(Vertex* parentNode);
+
+	/*!
+		\brief		observador de todos los vercices conectados a el pasado
+		\param		parentNode: puntero a vertice Vertex
+		\return		lista de punteros a vertices
+	*/
 	std::vector<Vertex*> getAllRelactioned(Vertex* parentNode);
 
+	/*!
+		\brief		modicifador de addicion de hijo
+		\param		ParentNode: puntero a vertice Vertex padre
+		\param		newSonNode: nuevo vertice hijo
+	*/
 	void addSon(Vertex* ParentNode, Vertex* newSonNode);
 
+	/*!
+		\brief		observador de todos los vercices del arbol
+		\return		lista de punteros a vertices
+	*/
 	std::vector<Vertex*> getAllVertexes();
-	std::vector<Vertex*> iGetAllVertexes(Vertex* base, std::vector<Vertex*> visited);
 
+	/*!
+		\brief		observador interno de todos los vertices de un nivel del arbol
+		\param		base: puntero a vertice base
+		\param		visited: lista de vertices visitados
+		\return		lista de punteros a vertices
+	*/
+	private: std::vector<Vertex*> iGetAllVertexes(Vertex* base, std::vector<Vertex*> visited);
+
+public:
+	/*!
+		\brief		observador de busqueda de un vertice del arbol
+		\param		vertex: puntero a vertive a buscar
+		\return		puntero a vertice del arbol
+	*/
 	Vertex* SearchVertex(Vertex* vertex);
 
+	/*!
+		\brief		escritor por pantalla del arbol
+	*/
 	void printTree();
-	void iPrint(Vertex* base);
 
+	/*!
+		\brief		escritor interno por pantalla de un nivel del arbol
+		\param		base: puntero a vertice base
+	*/
+	private: void iPrint(Vertex* base);
+
+public:
+	/*!
+		\brief		modificador de conexion de arboles
+		\param		headsT: lista de punteros a arboles de la stl
+		\return		booleano, verdadero si se han conectado al menos un par de arboles, falso de contrario
+	*/
 	bool tryConnect(std::vector<Tree*>& headsT);
+
+	/*!
+		\brief		observador de conexion de arboles
+		\param		t: arbol Tree
+		\return		booleano, verdadero si el par de arboles this y el parametro estan conectados, falso de contrario
+	*/
 	bool areConnected(Tree t);
 };
 
--- pr4/src/classes/Vertex.hpp
+++ pr4/src/classes/Vertex.hpp
@@ -14,40 +14,109 @@
 
 class Edge;
 
+//!< Definicion de la clase Vertex
 class Vertex {
 private:
-	std::string _flag;
-	Point2D _point;
-	std::vector<Edge*> _edges;
+	std::string _flag; //!< flag del vertice
+	Point2D _point; //!< punti del vertice
+	std::vector<Edge*> _edges; //lista de lados del vertice
 
 public:
+	/*!
+		\brief		constructor de la clase Vertex
+		\param		point: punto del vertice
+		\note			function inline
+	*/
 	inline Vertex (Point2D point=Point2D()) {
 		_point=point;
 		_flag="";
 	}
+
+	/*!
+		\brief		constructor de copiea de la clase Vertex
+		\param		vertex: vertice a copiar de
+		\note			function inline
+	*/
 	inline Vertex (const Vertex &vertex) {
 		_point=vertex.getPoint();
 		_flag=vertex.getFlag();
 		_edges=std::vector<Edge*>();
 	}
 
+	/*!
+		\brief		modificador de la flag
+		\param		flag: string de flag
+		\note			function inline
+	*/
 	inline void setFlag(std::string flag){_flag=flag;}
+
+	/*!
+		\brief		observador de la flag
+		\return		string de flag
+		\note			function inline
+		\note			funcion constante
+	*/
 	inline std::string getFlag() const {return _flag;}
 
+	/*!
+		\brief		modificador del punto
+		\param		point: nuevo punto
+		\note			function inline
+	*/
 	inline void setPoint(Point2D point) {_point=point;}
 
+	/*!
+		\brief		observador del punto
+		\return		punto
+		\note			function inline
+		\note			funcion constante
+	*/
 	inline Point2D getPoint() const {return _point;}
 
+	/*!
+		\brief		observador de lados
+		\return		vector de punteros a lados
+		\note			function inline
+		\note			funcion constante
+	*/
 	inline std::vector<Edge*> getEdges() const {return _edges;}
 
+	/*!
+		\brief		modificador de addicion de lado
+		\param		edge: nuevo lado (puntero)
+		\return verdadero si la operacion se llevo a cabo con exito, falso en el caso contrario
+	*/
 	bool addEdge(Edge* edge);
+
+	/*!
+		\brief		modificador de borrado de lado
+		\param		edge: lado a borrar (puntero)
+		\return verdadero si la operacion se llevo a cabo con exito, falso en el caso contrario
+	*/
 	bool removeEdge(Edge* Edge);
 
+	/*!
+		\brief		operador observador de igualdad
+		\param		vertex: vertice a comprobar con
+		\return		booleano, verdadero si son iguales, falso en el caso contrario
+	*/
 	bool operator==(const Vertex& vertex) const ;
+
+	/*!
+		\brief		operador observador de desigualdad
+		\param		vertex: vertice a comprobar con
+		\return		booleano, falso si son iguales, verdadero en el caso contrario
+		\note			funcion inline
+	*/
 	inline bool operator!=(const Vertex& vertex) const {
 		return (!(*this==vertex));
 	}
 
+	/*!
+		\brief		modificador de igualdad
+		\param		vertex: vertice a igualar con
+		\return		vertice
+	*/
 	Vertex& operator=(const Vertex& vertex);
 };
 
