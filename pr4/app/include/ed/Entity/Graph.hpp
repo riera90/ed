7483d68e6d86038ee6df3f40a412a3dcdffdefb8
diego@alpha <diegorodriguezriera@gmail.com>
2019-05-08 20:23:36 +0200

    git fuck

--- pr4/app/include/ed/Entity/Graph.hpp
+++ pr4/app/include/ed/Entity/Graph.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 template<class T_NODE, class T_EDGE>
 class Graph
@@ -15,7 +16,7 @@ private:
 public:
     Graph()
     {
-        this->nodes_ = {"Almeria","Cadiz","Cordoba","Granada","Huelva","Jaen","Malaga","Sevilla"};
+        /*this->nodes_ = {"Almeria","Cadiz","Cordoba","Granada","Huelva","Jaen","Malaga","Sevilla"};
         this->edges_ =
         {
             {32000, 32000, 32000, 166, 32000, 228, 219, 32000},
@@ -26,7 +27,7 @@ public:
             {228 ,32000 ,104 ,99 ,32000 ,32000 ,209 ,32000},
             {219 ,265 ,187 ,129 ,32000 ,209 ,32000 ,219},
             {32000 ,125 ,138 ,256 ,94 ,32000 ,219 ,320}
-        };
+        };*/
     }
     Graph(const Graph<T_NODE, T_EDGE> &g)
     {
@@ -35,18 +36,53 @@ public:
     }
 
 
-    Graph(const std::string &filePath)
+    Graph(const std::string &nodes_path, const std::string &edges_path)
     {
-        this->LoadFromFile(filePath);
+        this->LoadFromFile(nodes_path, edges_path);
     }
 
     ~Graph(){}
 
+    void clear(){this->edges_.clear();this->nodes_.clear();};
+
     std::vector<T_NODE> getNodes() const {return this->nodes_;};
     std::vector<std::vector<T_EDGE>> getEdges() const {return this->edges_;};
 
-    bool LoadFromFile(const std::string &filePath)
+    bool LoadFromFile(const std::string &nodes_path, const std::string &edges_path)
     {
+        std::ifstream nodes, edges;
+        edges.open(edges_path.c_str());
+        nodes.open(nodes_path.c_str());
+        char buffer[1024];
+
+        while (nodes.getline(buffer, 255, '\n')){
+            this->nodes_.push_back(buffer);
+        }
+
+        int pos, i = 0;
+        std::vector<T_EDGE> row;
+        T_EDGE value;
+        std::string buf;
+
+        while (edges.getline(buffer, 1024, '\n')){
+            std::cout <<"reading line!\n";
+            row.clear();
+            buf = buffer;
+            while (buf.find(' ')!=std::string::npos) {
+                std::cout <<"\treading field!\n";
+                value =  atoi(buf.substr(0, buf.find(' ')).c_str()); // that atoi is a little dirty, I know..., but hey, it works ¯\_(ツ)_/¯
+                buf =buf.substr(buf.find(' ') + 1, buf.size());
+                row.push_back(value);
+            }
+            std::cout <<"\treading field!\n";
+            value =  atoi(buf.substr(0, buf.find('\n')).c_str());
+            row.push_back(value);
+            this->edges_.push_back(row);
+            i++;
+        }
+
+        edges.close();
+        nodes.close();
         return true;
     }
 
@@ -64,10 +100,10 @@ public:
 
 
     void print() {
-        for (auto &node : this->getNodes()){
-            std::cout << node << ", ";
+        for (auto &node : this->nodes_){
+            std::cout << node << '\n';
         }
-        std::cout << std::endl;
+
 
         for (auto &row : this->getEdges()){
             for (auto &item : row){
--- pr4/app/src/main.cpp
+++ pr4/app/src/main.cpp
@@ -4,15 +4,73 @@
 #include <ed/Entity/Graph.hpp>
 #include <ed/Algorithm/algo.hpp>
 
+using namespace std;
+
+
+int menu()
+{
+    int opcion;
+
+    system("clear");
+    cout << "  0. Salir................................\n";
+    cout << "  1. Cargar grafo desde fichero...........\n";
+    cout << "  2. Algoritmo de Floyd (GD y GND)......\n";
+
+    cout << "Introduzca opcion...:";
+    cin >> opcion;
+
+    return opcion;
+}
+
 
 int main()
 {
-	std::cout << "hello, world\n";
-    Graph<std::string, int> graph;
+    Graph<string, int> g;
+    int opcion;
+    bool grafoIntroducido = false;
 
-    graph.print();
-    std::cout<<"--------------------\n";
-    floyd(graph);
+    do {
+        opcion = menu();
+        switch (opcion) {
+            case 1: { // Cargar grafo desde fichero
+                if (grafoIntroducido)
+                    g.clear(); // Si hay un grafo introducido se borra.
+                std::string edges, nodes;
+                std::cout << "enter the nodes file: ";
+                std::cin >> nodes;
+                std::cout << "enter the edges file: ";
+                std::cin >> edges;
+
+                nodes = "Andalucia.txt";edges = "matrizAndalucia.txt";
+
+                grafoIntroducido = g.LoadFromFile(nodes, edges);
+
+                if (grafoIntroducido) {
+                    cout << "Grafo cargado correctamente \n";
+                    //Prueba de la asignación y del destructor
+                    {
+                        Graph<string, int> g1 = g;
+                    }
+                } else
+                    cout << "Grafo no cargado \n";
+
+                getchar();
+                getchar();
+                break;
+            }
+
+            case 2: //Algoritmo de Floyd
+            {
+                if ( grafoIntroducido )
+                    floyd(g);
+                else
+                    cout << "Primero tiene que introducir un grafo\n";
+                getchar();
+                getchar();
+                break;
+            }
+        }
+    } while (opcion!=0);
 
     return 0;
 }
